568af264cdb9b35ac4444725903490f34fefd53a
Andres981020 <mosquera.andres@correounivalle.edu.co>
2020-08-07 23:09:54 -0500

    Mucho desorden para tan poco estilo

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import 'firebase/auth';
 import { useFirebaseApp, useUser } from 'reactfire';
+import { render } from '@testing-library/react';
+
 
 
 export default (props) =>{
@@ -26,26 +28,40 @@ export default (props) =>{
         <div>
             {
                 !user &&
-            <div>
-                <div >
-                <label htmlFor="email">Correo electronico:</label>
-                <input type ="email" id="email" onChange={(ev)=>setEmail(ev.target.value) } />
+                 
+        <div class="px-40 max-w-4xl" >
+            <div class="max-w rounded overflow-hidden shadow-lg">
+                <nav class="flex items-center justify-between flex-wrap bg-black p-2">
+                <span class="font-semibold text-white tracking-tight">What you see now</span>
+                </nav>
+            </div>
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Correo electronico" htmlFor="email">Correo electronico:</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Correo electronico" type ="email" id="email" onChange={(ev)=>setEmail(ev.target.value) } />
                 </div>
-                <div>
-                <label htmlFor="password">Contraseña:</label>
-                <input type ="password" id="password" onChange={(ev)=>setPassword(ev.target.value) }  />
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Contraseña" htmlFor="password">Contraseña:</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="****************" type ="password" id="password" onChange={(ev)=>setPassword(ev.target.value) }  />
                 </div>
                 <div >
-                <button onClick={submit} >  Crear cuenta </button>
+                    <button onClick={submit} class="bg-transparent text-blue-500 hover:text-blue-700 font-bold py-2 px-4 rounded-1" >  Crear cuenta </button>
                 </div>  
                 <div >
-                <button onClick={logint}> Iniciar sesion</button>
-                </div>     
+                    <button onClick={logint} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"> Iniciar sesion</button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Olvidaste tu clave?
+                    </a>
+                </div>  
+            </form>   
             </div>
             }
+
             {
-                user && <button onClick={logout}>Cerrrar sesion</button>
+                user && <button onClick={logout} class="bg-transparent text-blue-500 hover:text-blue-700 font-bold py-2 px-4 rounded-1">Cerrrar sesion</button>
             }
         </div>
+        
+       
     )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -30,8 +30,11 @@ export default class Tarjeta extends Component {
           </div>
           </div>
 
-          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-              Button
+          <button class="bg-transparent hover:bg-green-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Like
+          </button>
+          <button class="bg-transparent hover:bg-red-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Dislike
           </button>
 
       
